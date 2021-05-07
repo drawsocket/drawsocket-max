bc158b4c45001ac0c7ad8ce8e80670fbb124e28d
ramagottfried <rama.gottfried@gmail.com>
2021-05-07 11:20:27 +0200

    relesae system dev

--- make-realease.sh
+++ make-realease.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+node update-package-info.js
+
 mkdir drawsocket-max
 rsync -rv --exclude-from=./.copyignore * drawsocket-max
 tar zcvf drawsocket-max.tar.gz drawsocket-max
---
+++ update-package-info.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const data = JSON.parse(fs.readFileSync('package-info.json', 'utf8'))
+
+const filelist_dirs = ["docs", "examples", "extras", "help", "javascript", "patchers", "templates"];
+
+const getFiles = path => {
+    const files = []
+    for (const file of fs.readdirSync(path)) 
+    {
+        if( file[0] == '.' || file == "node_modules" )
+            continue;
+
+        const fullPath = path + '/' + file
+        if(fs.lstatSync(fullPath).isDirectory())
+            getFiles(fullPath).forEach(x => files.push(file + '/' + x))
+        else files.push(file)
+    }
+    return files
+}
+
+for( const f of filelist_dirs ){
+    data.filelist[f] = getFiles(f);
+}
+
+fs.writeFileSync('package-info.test.json', JSON.stringify(data, null, 4) )
